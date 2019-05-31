25567f74b0ed063baa66e98eb261e0eb808e15da
kachan28 <kochkinma@mail.ru>
2019-05-31 11:10:23 +0300

    Security update
    Security update

---
+++ VKCrypto/Utils.cs
@@ -0,0 +1,36 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Security;
+using System.Security.Cryptography;
+
+namespace VKCrypto
+{
+    public class Utils
+    {
+        public class Crypto
+        {
+            public class Sim
+            {
+                public class Encryptor
+                {
+
+                }
+                public class Decryptor
+                {
+
+                }
+            }
+
+            public class ASim
+            {
+                public class Encryptor
+                {
+
+                }
+                public class Decryptor
+                {
+
+                }
+            }
+        }
+    }
+}
